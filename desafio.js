1ffab51288dc63a2e39f697f0f1bee7f5564c7e9
alansolit <alancard2009@hotmail.com>
2024-07-10 17:38:44 -0300

    Create desafio.js

---
+++ desafio.js
@@ -0,0 +1,27 @@
+
+
+let heroName = "Alan";
+let level = "Prata";
+let level2 = "Bronze";
+let nivel1 = 1000;
+let nivel2 = 2000;
+let nivel3 = 5000;
+let nivel4 = 7000;
+let nivel5 = 8000;
+let nivel6 = 9000;
+let nivel7 = 10000;
+let nivel8 = 10001;
+
+if (nivel1 < nivel2) {
+    console.log("O herói de nome: " + heroName + " está no nível " + level);
+}
+else {
+    console.log("O herói de nome: " + heroName + " está no nível " + level2);
+}
+
+
+
+
+ 
+
+
